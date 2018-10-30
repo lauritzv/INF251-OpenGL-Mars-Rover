e431b502eeb3d64297b0a0ec9c59e9d562096373
lauritzv <lauritzv@gmail.com>
2018-10-30 13:32:29 +0100

    Camera class and tutor6 shaders

---
+++ MarsRover/Camera.cpp
@@ -0,0 +1,41 @@
+﻿#include "Camera.h"
+
+Matrix4f Camera::computeCameraTransform() const
+{
+	// camera rotation
+	Vector3f t = target.getNormalized();
+	Vector3f u = up.getNormalized();
+	Vector3f r = t.cross(u);
+	const Matrix4f camR(r.x(), r.y(), r.z(), 0.f,
+		u.x(), u.y(), u.z(), 0.f,
+		-t.x(), -t.y(), -t.z(), 0.f,
+		0.f, 0.f, 0.f, 1.f);
+
+	// camera translation
+	Matrix4f camT = Matrix4f::createTranslation(-position);
+
+	// perspective projection
+	Matrix4f prj = Matrix4f::createPerspectivePrj(fov, ar, zNear, zFar);
+
+	// scaling due to zooming
+	Matrix4f camZoom = Matrix4f::createScaling(zoom, zoom, 1.f);
+
+	// Final transformation. Notice the multiplication order
+	// First vertices are moved in camera space
+	// Then the perspective projection puts them in clip space
+	// And a final zooming factor is applied in clip space
+	return camZoom * prj * camR  * camT;
+
+} /* computeCameraTransform() */
+
+void Camera::init()
+{
+	position.set(0.f, 0.f, 0.f);
+	target.set(0.f, 0.f, -1.f);
+	up.set(0.f, 1.f, 0.f);
+	fov = 30.f;
+	ar = 1.f; // will be correctly initialized in the "display()" method
+	zNear = 0.1f;
+	zFar = 100.f;
+	zoom = 1.f;
+}
---
+++ MarsRover/Camera.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "Vector3.h"
+#include "Matrix4.h"
+
+class Camera
+{
+public:
+	Vector3f position;	///< the position of the camera
+	Vector3f target;	///< the direction the camera is looking at
+	Vector3f up;		///< the up vector of the camera
+
+	float fov;			///< camera field of view
+	float ar;			///< camera aspect ratio
+	float zNear, zFar;	///< depth of the near and far plane
+	float zoom;			///< an additional scaling parameter
+
+	Matrix4f computeCameraTransform() const;
+	void init();
+};
