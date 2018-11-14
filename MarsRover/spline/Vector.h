e9482e6b96289d754745c337865486f71a5550f0
lauritzv <lauritzv@gmail.com>
2018-11-14 20:58:39 +0100

    minor cleanup

--- MarsRover/spline/Vector.h
+++ MarsRover/spline/Vector.h
@@ -10,40 +10,30 @@ public:
 	double y;
 	double z;
 
-public:
 	Vector();
 	Vector(double _x, double _y, double _z);
 	Vector(const Vector& rhs);
 	Vector& operator=(const Vector& rhs);
 
-public:
 	Vector cross(const Vector& rhs) const;
 	Vector normalize() const;
 	double dot(const Vector& rhs) const;
 	double lengthSq() const;
 
-public:
 	Vector Truncate(double max_value) const;
 
-public:
 	Vector& operator-=(const Vector& rhs);
 	Vector& operator+=(const Vector& rhs);
 	Vector& operator*=(double value);
 	Vector& operator/=(double value);
 
-public:
 	bool operator <=(const Vector& rhs) const;
 	bool operator >=(const Vector& rhs) const;
 	bool operator ==(const Vector& rhs) const;
 	bool operator !=(const Vector& rhs) const;
 
-public:
 	void reset() { x=0; y=0; z=0; }
-
-public:
 	std::string toString() const;
-
-public:
 	double length() const;
 };
 
